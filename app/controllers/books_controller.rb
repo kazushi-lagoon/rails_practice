196e7d3f038ec177fbc24a87a7fc28911a8a4bcb
kazushi-lagoon <kazu0226_7382@yahoo.co.jp>
2020-06-28 18:15:36 +0900

    internationalization

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,8 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        success = t('.success')
+        format.html { redirect_to @book, notice: success }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
@@ -42,7 +43,8 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        success = t('.success')
+        format.html { redirect_to @book, notice: success }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
@@ -56,7 +58,8 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      success = t('.success')
+      format.html { redirect_to books_url, notice: success }
       format.json { head :no_content }
     end
   end
